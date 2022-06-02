118c22410f30b6b14f114a4f463e470a4c646632
Hemalatha-kola <kola123hema@gmail.com>
2022-06-02 16:14:59 -0700

    Lab-3 done

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
-function NewSkillForm(){
+function NewSkillForm({addSkill}){
+
     return(
+        <>
         <form className="NewSkillForm">
             <label>Skill</label>
-            <input></input>
+            <input 
+            />
+            
             <label>Level</label>
             <select>
-                <option>5</option>
-                <option>4</option>
-                <option>3</option>
-                <option>2</option>
-                <option>1</option>
+                <option >5</option>
+                <option >4</option>
+                <option >3</option>
+                <option >2</option>
+                <option >1</option>
             </select>
             <button>ADD SKILL</button>
         </form>
+        </>
     );
 }
 
